3e8d57a964a75be4481d0d0b2c0fbb9e6481a3c4
mykeld74 <mike@msdwebsolutions.com>
2025-06-06 14:32:22 -0600

    update the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,14 @@
 	import '$css/styles.css';
 </script>
 
+<svelte:head>
+	<title>Bridge Capital</title>
+	<meta name="description" content="Bridge Capital" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#12213e" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+	<meta name="apple-mobile-web-app-title" content="Bridge Capital" />
+</svelte:head>
+
 {@render children()}
